4770f5a1e3260f96a3eaca94b5576202029c01f7
bahaly <50265605+bahaly@users.noreply.github.com>
2022-09-19 16:55:57 +0000

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+$env:POSTGRES_USERNAME='postgres'
+$env:POSTGRES_PASSWORD='password'
+$env:POSTGRES_HOST='database-2.c3g6j55foqay.us-east-1.rds.amazonaws.com'
+$env:POSTGRES_DB='postgres'
+$env:AWS_BUCKET='arn:aws:s3:::test-214486697737-dev'
+$env:AWS_REGION='us-east-1'
+$env:AWS_PROFILE='default'
+$env:JWT_SECRET='testing'
+$env:URL='http://localhost:8100'
