e38a766ba367d38dc0b9cdb5b6bad77a25a9d31f
bahaly <50265605+bahaly@users.noreply.github.com>
2022-09-22 22:13:28 +0000

    reWork dockerfile and conf file

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 $env:POSTGRES_USERNAME='postgres'
 $env:POSTGRES_PASSWORD='password'
-$env:POSTGRES_HOST='database-1.codnnjxwl9f9.us-east-1.rds.amazonaws.com'
+$env:POSTGRES_HOST='database-1.c2rxsat2m3rl.us-east-1.rds.amazonaws.com'
 $env:POSTGRES_DB='postgres'
-$env:AWS_BUCKET='arn:aws:s3:::bucket-280862260255-dev'
+$env:AWS_BUCKET='arn:aws:s3:::bucket-966002337355-dev'
 $env:AWS_REGION='us-east-1'
 $env:AWS_PROFILE='default'
 $env:JWT_SECRET='testing'
